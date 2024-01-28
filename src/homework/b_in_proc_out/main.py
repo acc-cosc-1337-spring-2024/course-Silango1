886a4aba15e4cf4efa7c2b88a6db7a8eb74ed8e9
Silango1 <157082554+Silango1@users.noreply.github.com>
2024-01-28 02:21:43 +0000

    Assignment: Homework 1

--- src/homework/b_in_proc_out/main.py
+++ src/homework/b_in_proc_out/main.py
@@ -1,1 +1,10 @@
 #
+import output
+result = output.multiply_numbers(5,5)
+print(result)
+
+value1 = input("Enter the value 1: ")
+value2 = input("Enter the value 2: ")
+
+result = output.multiply_numbers(int(value1),int(value2))
+print(result)
--- src/homework/b_in_proc_out/output.py
+++ src/homework/b_in_proc_out/output.py
@@ -1,2 +1,5 @@
 def get_number(num):
     return num
+def multiply_numbers(num1, num2):
+    MulNum = num1 * num2 #Create local function/method variable to multiply 2 numbers
+    return MulNum
