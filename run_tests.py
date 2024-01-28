436e48410b3f0d16174c2e15b19705c4d498e8e6
Silango1 <157082554+Silango1@users.noreply.github.com>
2024-01-28 02:34:15 +0000

    Updated comments in run test py file

--- run_tests.py
+++ run_tests.py
@@ -3,6 +3,7 @@ import unittest
 the file in /tests/homework/b_in_proc_out/tests_in_proc_out
 has the test functions
 '''
+#aDDING IMPORT FOR MULTIPLY NUMBERS
 from tests.homework.b_in_proc_out import tests_in_proc_out
 
 suite = unittest.TestLoader().loadTestsFromModule(tests_in_proc_out)
